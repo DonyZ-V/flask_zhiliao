b7229942d871cec448617f3c82cbbf3018f264ed
DonyZ <chinazxd1993@163.com>
2017-10-07 15:42:34 +0800

    功能基本完成

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from flask_zhiliao import app
 from exts import db
-from models import User
+from models import User, Question, Answer
 
 manage = Manager(app)
 
